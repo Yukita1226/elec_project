4e4ef57f97f50f58d87ff1f237df07fe592a207a
Yukita1226 <thammarat.songwiwat@gmail.com>
2026-04-16 22:41:16 +0700

    add global

--- backend/controller/cont.go
+++ backend/controller/cont.go
@@ -1,2 +1,27 @@
 package controller
 
+import (
+	"net/http"
+
+	"ev/entity"
+	"github.com/gin-gonic/gin"
+
+	"math/rand/v2"
+)
+
+var solar =  entity.Solar{}
+var	ev  = entity.Evstation{}
+var car = entity.Car{}
+
+
+func makenum()
+{
+
+
+}
+func Getsolar(c * gin.Context){
+
+	test = 
+	c.JSON(http.StatusOK);
+	
+}
