dddd6e63e65187ec71a16ce63437557048e2e5c5
Yukita1226 <thammarat.songwiwat@gmail.com>
2026-04-19 22:20:06 +0700

    hi

--- front/api/car.cpp
+++ front/api/car.cpp
@@ -1,8 +1,17 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
+#include "json.hpp"
 #include "api.h"
-#include <QJsonDocument>
-#include <QJsonObject>
-#include <QByteArray>
+#include "struct/car.h"
+
+using js = nlohmann::json;
+
+size_t Wcallb(void* contents, size_t size, size_t nmemb, std::string* s) {
+    s->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
 
 void Recivedata::getCar() {
-    std::cout << "fetch solar data" << std::endl;
+   
 }
---
+++ front/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "test ok\n";
+    return 0;
+}
