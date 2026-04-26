c9f67fe60f2490f17096999160257ecd18d6227b
Yukita1226 <thammarat.songwiwat@gmail.com>
2026-04-26 11:00:17 +0700

    dix

--- front/main.cpp
+++ front/main.cpp
@@ -13,18 +13,13 @@
 #include "../ui/page.h"
 
 int main(int argc, char* argv[]) {
-    // Initialize Core and Drivers
+
     lv_init();
     lv_display_t * disp = lv_sdl_window_create(800, 480);
     lv_indev_t * mouse = lv_sdl_mouse_create();
 
-    // --- LOAD THE UI ---
-    // This calls the function we built in t2.cpp
     create_page_t2();
 
-    std::cout << "Success! Window open and UI loaded from t2.cpp." << std::endl;
-
-    // The Main Loop
     while(1) {
         lv_timer_handler();
         Sleep(5);
@@ -33,3 +28,5 @@ int main(int argc, char* argv[]) {
     return 0;
 }
 //g++ main.cpp api/*.cpp ui/t2/*.cpp -I. -I./lvgl -I./lvgl/src -I./lvgl/src/drivers/sdl -DLV_CONF_INCLUDE_SIMPLE -DLV_USE_SDL=1 -L. -o app.exe "-Wl,--start-group" liblvgl.a -lcurl -lSDL2 "-Wl,--end-group"
+
+//g++ main.cpp api/*.cpp ui/t2/*.cpp -I. -I./lvgl -I./lvgl/src -I./lvgl/src/drivers/sdl -DLV_CONF_INCLUDE_SIMPLE -DLV_USE_SDL=1 -L. -o app.exe "-Wl,--start-group" liblvgl.a -lSDL2 -lwebsockets -lws2_32 "-Wl,--end-group"
